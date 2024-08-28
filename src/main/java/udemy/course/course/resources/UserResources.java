346ae80bbda2e0ac427033b9786c36551ff8cbfb
Etanielcaus <nielfilosofia@gmail.com>
2024-08-27 21:40:34 -0300

    Entidade User e seu resource

---
+++ src/main/java/udemy/course/course/resources/UserResources.java
@@ -0,0 +1,18 @@
+package udemy.course.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import udemy.course.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "joao", "joao@joao", "99999", "123");
+        return ResponseEntity.ok().body(u);
+    }
+}
